54396a8ab1917a4966e2a2a390801083993a7c47
thamizh-code <pathamizhpaana@gmail.com>
2024-07-19 14:05:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_route_table" "public_rt" {
 
 resource "aws_route" "routing" {
   route_table_id = aws_route_table.public_rt.id
-  destination_cidr_block = "0.0.0.0./0"
+  destination_cidr_block = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.my_gw.id
 }
 
