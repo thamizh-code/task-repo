7a2b20359067ec63d079744099d2badd70fc4df1
thamizh-code <pathamizhpaana@gmail.com>
2024-07-19 14:02:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_vpc" "my_vpc" 
+resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "pub_subnet"
+resource "aws_subnet" "pub_subnet" {
   vpc_id = aws_vpc.my_vpc.id
   cidr_block = "10.0.1.0/24"
 }
